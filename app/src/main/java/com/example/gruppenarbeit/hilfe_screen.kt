1daf485561d88756527e84e22aac5d5bd7bd5610
jerekristian <sws321321>
2019-05-21 15:13:17 +0200

    Hilfescreen optimiert

--- app/src/main/java/com/example/gruppenarbeit/hilfe_screen.kt
+++ app/src/main/java/com/example/gruppenarbeit/hilfe_screen.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 
 class hilfe_screen : AppCompatActivity() {
 
@@ -19,6 +21,27 @@ class hilfe_screen : AppCompatActivity() {
             android.R.layout.simple_spinner_dropdown_item,
             resources.getStringArray(R.array.Calculations)
         )
+
+        dropdownHilfe.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedcalc = findViewById<Spinner>(R.id.spinner_hilfe).selectedItem.toString()
+
+                if (selectedcalc == "Addition") {
+                    findViewById<TextView>(R.id.textView_additional).text = getString(R.string.Hilftext1)
+                } else if (selectedcalc == "Subtraktion") {
+                    findViewById<TextView>(R.id.textView_additional).text = getString(R.string.Hilftext2)
+                } else if (selectedcalc == "Multiplikation") {
+                    findViewById<TextView>(R.id.textView_additional).text = getString(R.string.Hilftext3)
+                } else if (selectedcalc == "Division") {
+                    findViewById<TextView>(R.id.textView_additional).text = getString(R.string.Hilftext4)
+                }
+            }
+
+        }
     }
 
     fun tohome(view: View) {
